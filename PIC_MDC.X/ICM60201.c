e2811274c2a447b52040e09d019321095d3720f9
reo kashiyama <31460945+reokashiyama@users.noreply.github.com>
2018-05-17 15:01:24 +0900

    Update ICM20601.c - Please read FIXME, XXX, TODO

--- PIC_MDC.X/ICM60201.c
+++ PIC_MDC.X/ICM60201.c
@@ -18,7 +18,7 @@ const UBYTE ACCEL_CONFIG2   = 0x1D;
 const UBYTE FIFO_EN         = 0x23;
 const UBYTE INT_PIN_CONFIG  = 0x37;
 const UBYTE INT_ENABLE      = 0x38;
-const UBYTE INT_STATUS      = 0x39;
+const UBYTE INT_STATUS      = 0x3A; 
 const UBYTE PWR_MGMT_1      = 0x6B;
 const UBYTE PWR_MGMT_2      = 0x6C;
 
@@ -52,9 +52,13 @@ int initICM()
     if(ans == 0){
             __delay_us(2000);
             writeAddr(PWR_MGMT_1,0x01);     //clock : PLL
+            //  TODO    : need to consider about DLPF settings
             writeAddr(CONFIG,0x00);         //FSYNC:disabled
+            //  XXX     : change the range of gyro sensor to ±4000dps (degree per sec)
             writeAddr(GYRO_CONFIG,0x00);    //FS:500 deg/sec
+            //  TODO    : need to confirm the range of accelemometer
             writeAdde(ACCEL_CONFIG,0x00);   //FS:4g
+            //  TODO    : need to consider about DLPF setting
             writeAddr(ACCEL_CONFIG2,0x00);  //averaging 4 samples, DLPF : 218Hz
             __delay_us(2000);
     }else ans == -1;
@@ -64,10 +68,12 @@ int initICM()
 int readICM(UBYTE *data, int offset)
 {
     int ans , i , ack ;
-
-    while(ans != 0xE5){
+    
+    //  FIXME   : read datasheet of MPU9250 (P.30 register map), and ICM20601(P.40)
+    //            DATA_RDY_INT(bit0 of INT_STATUS) clears to 0 after the register has been read. 
+    while(ans != 0x01){
         ans = readAddr(INT_STATUS);
-        ans = ans & 0xE5;
+        ans = ans & 0x01;
     }
 
     ans = startI2C(ICM_ADDR,RW_0);
